5d25636ce04638d2bd8adec7af5b1451b7d86eb5
jisuengkim <dickseung23@naver.com>
2023-03-29 16:32:02 +0900

    함수 모듈화

---
+++ script/input.js
@@ -0,0 +1,49 @@
+import{inlineButtonModify,inlineButtonDelete} from './inlineButton.js'
+//리스트 입력 받는 함수(버튼클릭 이벤트)
+function inputList() {
+    const $listAddbar = document.getElementById('list-add-bar');
+    const $addButton = document.getElementById('addButton');
+    // console.log($listAddbar);
+
+    $addButton.onclick = e => {
+        // console.log('추가버튼클릭함!');
+        //입력값 받아서 dom값 생성하고 ul리스트에 추가하기'
+        if ($listAddbar.value === "") {
+            alert('할일을 입력하세요!');
+            $listAddbar.focus();
+        } else {
+            //ul 값 가져오기
+            const $listContainer = document.getElementById('#list-container');
+            // console.log($listContainer);
+            // console.log($listAddbar.value);
+            //div태그 생성
+            //만든 div태그에 class부여
+            //입력받은 값 집어넣기
+            // $listContainer.appendChild($listValue);
+            $listContainer.innerHTML += "<li><div class='list section'>" + $listAddbar.value + "</div><a href='#' id='inli-modify' title='Button push blue/green' class='button btnPush btnBlueGreen button-inli'>수정</a><a href='#' id='inli-delete' title='Button push blue/green' class='button btnPush btnRed button-inli'>삭제</a></li>";
+            $listAddbar.value = "";
+            inlineButtonDelete();
+            inlineButtonModify();
+        }
+    }
+};
+//리스트 입력받는 함수(엔터키 이벤트)
+function inputEnterList() {
+    const $listAddbar = document.getElementById('list-add-bar');
+
+    $listAddbar.onkeyup = e => {
+        if (e.key === "Enter") {
+            if ($listAddbar.value === "") {
+                alert('할 일을 작성하세요');
+            } else {
+                const $listContainer = document.getElementById('#list-container');
+                $listContainer.innerHTML += "<li><div class='list section'>" + $listAddbar.value + "</div><a href='#' id='inli-modify' title='Button push blue/green' class='button btnPush btnBlueGreen button-inli'>수정</a><a href='#' id='inli-delete' title='Button push blue/green' class='button btnPush btnRed button-inli'>삭제</a></li>";
+                $listAddbar.value = "";
+                inlineButtonDelete();
+                inlineButtonModify();
+            }
+        }
+    }
+};
+
+export{inputList,inputEnterList};
